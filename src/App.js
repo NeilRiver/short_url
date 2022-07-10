b6a8dd729f5d06ad87dd78b4d25ebd934022b9dd
Nick <nelo-001@mail.ru>
2022-07-10 18:22:21 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Sing_in from './components/Sign_in';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      12
+      <Sing_in/>
+    </div>
+  );
+}
+
+export default App;
