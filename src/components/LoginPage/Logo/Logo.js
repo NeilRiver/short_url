eb2ab0f07f92ad3a32d7f472d66226531ce2c023
Nick <nelo-001@mail.ru>
2022-07-16 17:32:42 +0300

    changed the value of prop to from redux.store

--- src/components/LoginPage/Logo/Logo.js
+++ src/components/LoginPage/Logo/Logo.js
@@ -4,6 +4,7 @@ import { styled } from '@mui/material/styles';
 
 import logo from '../../../static/logo.png'
 import sing_up_logo from '../../../static/signup-logo.png'
+import { useSelector } from 'react-redux';
 
 const Img = styled('img')({
     margin: 'auto',
@@ -13,10 +14,11 @@ const Img = styled('img')({
 });
 
 export default function Logo(prop) {
+    const sign_up = useSelector(state => state.app.sign_up)
     return (
         <Grid item>
             <ButtonBase sx={{ width: 328, height: 128  }}>
-                <Img alt="complex" src={ prop.sing_up ? logo : sing_up_logo } />
+                <Img alt="complex" src={ sign_up ? logo : sing_up_logo } />
             </ButtonBase>
         </Grid>
     )
